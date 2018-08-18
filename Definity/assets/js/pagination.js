924d2b9e105f4e49cea8f49fd8b92a0f62985abc
Lewis, Greg (DET-MRM) <Greg.Lewis@mrm-mccann.com>
2018-08-18 05:38:01 -0400

    pagination working completely.

--- Definity/assets/js/pagination.js
+++ Definity/assets/js/pagination.js
@@ -1,5 +1,58 @@
 (function ($) {
 
+
+
+
+
+
+
+
+    var behind = $('.nav-btns .prev');
+    var forward = $('.nav-btns .next');
+
+    function prev(current, pages) {
+        var index = pages.indexOf(current);
+        if (index === 0) {
+            return pages[pages.length - 1];
+        }
+        return pages[index - 1];
+    }
+
+    function next(current, pages) {
+        var index = pages.indexOf(current);
+        if (index === pages.length - 1) {
+            return pages[0];
+        }
+        return pages[index + 1];
+    }
+
+    behind.on('click', function () {
+        var pages = [
+             '/Digitaldimensionz_Redesign/Definity/pages/portfolio/portfolio-single-1.html',
+             '/Digitaldimensionz_Redesign/Definity/pages/portfolio/portfolio-single-1-2.html',
+             '/Digitaldimensionz_Redesign/Definity/pages/portfolio/portfolio-single-1-3.html'
+        ];
+        var newUrl = prev(document.location.pathname, pages);
+        window.open(newUrl, '_self');
+
+    });
+
+    forward.on('click', function () {
+        var pages = [
+              '/Digitaldimensionz_Redesign/Definity/pages/portfolio/portfolio-single-1.html',
+              '/Digitaldimensionz_Redesign/Definity/pages/portfolio/portfolio-single-1-2.html',
+              '/Digitaldimensionz_Redesign/Definity/pages/portfolio/portfolio-single-1-3.html'
+        ];
+        var newUrl = next(document.location.pathname, pages);
+        /*window.open(newUrl);*/
+        window.open(newUrl, '_self');
+        /*window.location.href = newUrl;*/
+
+    });
+
+
+
+/*
     $('.nav-btns a').on('click', function (e) {
 
         function getUrlParam(name) {
@@ -32,6 +85,6 @@
             }
         }
 
-    });
+    });*/
 
 })($);
