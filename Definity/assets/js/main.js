0872dff976f744d5c60ec88ae1d64f94e81eaf29
Lewis, Greg (DET-MRM) <Greg.Lewis@mrm-mccann.com>
2018-09-24 15:16:53 -0400

    edits with servicrs page

--- Definity/assets/js/main.js
+++ Definity/assets/js/main.js
@@ -426,198 +426,245 @@
 	function initSliders() {
 
 		// Features Slider
-		$('.ft-slider').slick({
-			autoplay: true,
-			autoplaySpeed: 4000,
-			slidesToShow: 3,
-			slidesToScroll: 1,
-			dots: false,
-			arrows: true,
-			prevArrow: '<button type="button" class="info-slider-nav slick-prev"><i class="fa fa-long-arrow-left"></i></button>',
-			nextArrow: '<button type="button" class="info-slider-nav slick-next"><i class="fa fa-long-arrow-right"></i></button>',
-			responsive: [
-			    {
-			      breakpoint: 999,
-			      settings: {
-			        slidesToShow: 2,
-			        slidesToScroll: 2,
-			        infinite: true,
-			      }
-			    },
-			    {
-			      breakpoint: 770,
-			      settings: {
-			        slidesToShow: 1,
-			        slidesToScroll: 1
-			      }
-			    }
-			  ]
-		});
 
-		// Testimonials Sliders
-		$('.t-slider').slick({
-			autoplay: false,
-			autoplaySpeed: 4000,
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			dots: false,
-			arrows: true,
-			prevArrow: '<button type="button" class="t-slider-nav slick-prev"><span class="linea-arrows-slim-left"></span></button>',
-			nextArrow: '<button type="button" class="t-slider-nav slick-next"><span class="linea-arrows-slim-right"></span></button>',
-		});
 
-		// Brands/Clients Slider
-		$('.clients-slider').slick({
-			autoplay: true,
-			autoplaySpeed: 2500,
-			slidesToShow: 4,
-			slidesToScroll: 1,
-			dots: false,
-			fade: false,
-			arrows: true,
-			responsive: [
-			    {
-			      breakpoint: 999,
-			      settings: {
-			        slidesToShow: 3,
-			        slidesToScroll: 2,
-			        infinite: true,
-			      }
-			    },
-			    {
-			      breakpoint: 850,
-			      settings: {
-			        slidesToShow: 2,
-			        slidesToScroll: 1
-			      }
-			    },
-			    {
-			      breakpoint: 478,
-			      settings: {
-			        slidesToShow: 2,
-			        slidesToScroll: 1,
-					  arrows: false,
-					  autoplaySpeed: 2000
 
-				  }
-			    }
-			]
-		});
 
-		// Portfolio Single Slider
-		$('.single-img-slider').slick({
-			autoplay: true,
-			autoplaySpeed: 4000,
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			adaptiveHeight: true,
-			dots: false,
-			arrows: true,
-			prevArrow: '<button type="button" class="slider-nav sl-prev"><span class="linea-arrows-slim-left"></span></button>',
-			nextArrow: '<button type="button" class="slider-nav sl-next"><span class="linea-arrows-slim-right"></span></button>',
-		});
+		if ( $('.ft-slider').length ) {
+			$('.ft-slider').slick({
+				autoplay: true,
+				autoplaySpeed: 4000,
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				dots: false,
+				arrows: true,
+				prevArrow: '<button type="button" class="info-slider-nav slick-prev"><i class="fa fa-long-arrow-left"></i></button>',
+				nextArrow: '<button type="button" class="info-slider-nav slick-next"><i class="fa fa-long-arrow-right"></i></button>',
+				responsive: [
+					{
+						breakpoint: 999,
+						settings: {
+							slidesToShow: 2,
+							slidesToScroll: 2,
+							infinite: true,
+						}
+					},
+					{
+						breakpoint: 770,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1
+						}
+					}
+				]
+			});
+		}
 
-		// Centered Gallery
-		$('.centered-gallery').slick({
-			centerMode: true,
-			  centerPadding: '60px',
-			  slidesToShow: 3,
-			  responsive: [
-			    {
-			      breakpoint: 768,
-			      settings: {
-			        arrows: false,
-			        centerMode: true,
-			        centerPadding: '40px',
-			        slidesToShow: 3
-			      }
-			    },
-			    {
-			      breakpoint: 480,
-			      settings: {
-			        arrows: false,
-			        centerMode: true,
-			        centerPadding: '40px',
-			        slidesToShow: 1
-			      }
-			    }
-			  ]
-		});
 
-		// Full Screen Hero Slider
-		$('.fs-slider').slick({
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			useCSS: true,
-			fade: true,
-			dots: false,
-			arrows: true,
-			prevArrow: '<button type="button" class="slick-prev"><span class="linea-arrows-slim-left"></span></button>',
-			nextArrow: '<button type="button" class="slick-next"><span class="linea-arrows-slim-right"></span></button>',
-			autoplay: true,
-			autoplaySpeed: 4000,
-		});
 
-		// Full Width Hero Slider
-		$('.fw-slider').slick({
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			fade: true,
-			dots: false,
-			arrows: true,
-			prevArrow: '<button type="button" class="slick-prev"><span class="linea-arrows-slim-left"></span></button>',
-			nextArrow: '<button type="button" class="slick-next"><span class="linea-arrows-slim-right"></span></button>',
-			autoplay: true,
-			autoplaySpeed: 4000,
-		});
+		if ($('.t-slider').length) {
+			// Testimonials Sliders
+			$('.t-slider').slick({
+				autoplay: false,
+				autoplaySpeed: 4000,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				dots: false,
+				arrows: true,
+				prevArrow: '<button type="button" class="t-slider-nav slick-prev"><span class="linea-arrows-slim-left"></span></button>',
+				nextArrow: '<button type="button" class="t-slider-nav slick-next"><span class="linea-arrows-slim-right"></span></button>',
+			});
+		}
 
-		// Text Slider
-		$('.text-slider').slick({
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			adaptiveHeight: true,
-			speed: 300,
-			fade: true,
-			dots: false,
-			arrows: true,
-			prevArrow: '<button type="button" class="slick-prev"><span class="linea-arrows-slim-left"></span></button>',
-			nextArrow: '<button type="button" class="slick-next"><span class="linea-arrows-slim-right"></span></button>',
-			autoplay: true,
-			autoplaySpeed: 4000,
-		});
 
-		// Shop Product Slider
-		$('.shop-p-slider').slick({
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			adaptiveHeight: true,
-			speed: 300,
-			// fade: false,
-			dots: false,
-			arrows: true,
-			prevArrow: '<button type="button" class="shop-p-slider-nav shop-p-slider-nav-left"><span class="linea-arrows-slim-left"></span></button>',
-			nextArrow: '<button type="button" class="shop-p-slider-nav shop-p-slider-nav-right"><span class="linea-arrows-slim-right"></span></button>',
-			autoplay: false,
-		});
 
-		// Shop Product Single - Slider
-		$('.prod_single_img_slider').slick({
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			adaptiveHeight: true,
-			speed: 300,
-			// fade: false,
-			infinite: true,
-			dots: true,
-			arrows: true,
-			prevArrow: '<button type="button" class="shop-p-slider-nav shop-p-slider-nav-left"><span class="linea-arrows-slim-left"></span></button>',
-			nextArrow: '<button type="button" class="shop-p-slider-nav shop-p-slider-nav-right"><span class="linea-arrows-slim-right"></span></button>',
-			autoplay: false,
-			accessibility: false,
-			customPaging: function (slider, i) {
-	            return '<a href="#">' + $('.prod_single_thumbs_slider li:nth-child(' + (i + 1) + ')').html() + '</a>';
-	        }
-		});
+
+			if ($('.clients-slider').length) {
+				// Brands/Clients Slider
+				$('.clients-slider').slick({
+					autoplay: true,
+					autoplaySpeed: 2500,
+					slidesToShow: 4,
+					slidesToScroll: 1,
+					dots: false,
+					fade: false,
+					arrows: true,
+					responsive: [
+						{
+							breakpoint: 999,
+							settings: {
+								slidesToShow: 3,
+								slidesToScroll: 2,
+								infinite: true,
+							}
+						},
+						{
+							breakpoint: 850,
+							settings: {
+								slidesToShow: 2,
+								slidesToScroll: 1
+							}
+						},
+						{
+							breakpoint: 478,
+							settings: {
+								slidesToShow: 2,
+								slidesToScroll: 1,
+								arrows: false,
+								autoplaySpeed: 2000
+
+							}
+						}
+					]
+				});
+			}
+
+
+
+
+			if ($('.single-img-slider').length) {
+				// Portfolio Single Slider
+				$('.single-img-slider').slick({
+					autoplay: true,
+					autoplaySpeed: 4000,
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					adaptiveHeight: true,
+					dots: false,
+					arrows: true,
+					prevArrow: '<button type="button" class="slider-nav sl-prev"><span class="linea-arrows-slim-left"></span></button>',
+					nextArrow: '<button type="button" class="slider-nav sl-next"><span class="linea-arrows-slim-right"></span></button>',
+				});
+			}
+
+
+
+			if ($('.centered-gallery').length) {
+				// Centered Gallery
+				$('.centered-gallery').slick({
+					centerMode: true,
+					centerPadding: '60px',
+					slidesToShow: 3,
+					responsive: [
+						{
+							breakpoint: 768,
+							settings: {
+								arrows: false,
+								centerMode: true,
+								centerPadding: '40px',
+								slidesToShow: 3
+							}
+						},
+						{
+							breakpoint: 480,
+							settings: {
+								arrows: false,
+								centerMode: true,
+								centerPadding: '40px',
+								slidesToShow: 1
+							}
+						}
+					]
+				});
+			}
+
+
+
+
+			if ($('.fs-slider').length) {
+				// Full Screen Hero Slider
+				$('.fs-slider').slick({
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					useCSS: true,
+					fade: true,
+					dots: false,
+					arrows: true,
+					prevArrow: '<button type="button" class="slick-prev"><span class="linea-arrows-slim-left"></span></button>',
+					nextArrow: '<button type="button" class="slick-next"><span class="linea-arrows-slim-right"></span></button>',
+					autoplay: true,
+					autoplaySpeed: 4000,
+				});
+			}
+
+
+
+			if ($('.fw-slider').length) {
+				// Full Width Hero Slider
+				$('.fw-slider').slick({
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					fade: true,
+					dots: false,
+					arrows: true,
+					prevArrow: '<button type="button" class="slick-prev"><span class="linea-arrows-slim-left"></span></button>',
+					nextArrow: '<button type="button" class="slick-next"><span class="linea-arrows-slim-right"></span></button>',
+					autoplay: true,
+					autoplaySpeed: 4000,
+				});
+			}
+
+
+
+			if ($('.text-slider').length) {
+				// Text Slider
+				$('.text-slider').slick({
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					adaptiveHeight: true,
+					speed: 300,
+					fade: true,
+					dots: false,
+					arrows: true,
+					prevArrow: '<button type="button" class="slick-prev"><span class="linea-arrows-slim-left"></span></button>',
+					nextArrow: '<button type="button" class="slick-next"><span class="linea-arrows-slim-right"></span></button>',
+					autoplay: true,
+					autoplaySpeed: 4000,
+				});
+			}
+
+
+
+			if ($('.shop-p-slider').length) {
+				// Shop Product Slider
+				$('.shop-p-slider').slick({
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					adaptiveHeight: true,
+					speed: 300,
+					// fade: false,
+					dots: false,
+					arrows: true,
+					prevArrow: '<button type="button" class="shop-p-slider-nav shop-p-slider-nav-left"><span class="linea-arrows-slim-left"></span></button>',
+					nextArrow: '<button type="button" class="shop-p-slider-nav shop-p-slider-nav-right"><span class="linea-arrows-slim-right"></span></button>',
+					autoplay: false,
+				});
+			}
+
+
+
+			if ($('.prod_single_img_slider').length) {
+				// Shop Product Single - Slider
+				$('.prod_single_img_slider').slick({
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					adaptiveHeight: true,
+					speed: 300,
+					// fade: false,
+					infinite: true,
+					dots: true,
+					arrows: true,
+					prevArrow: '<button type="button" class="shop-p-slider-nav shop-p-slider-nav-left"><span class="linea-arrows-slim-left"></span></button>',
+					nextArrow: '<button type="button" class="shop-p-slider-nav shop-p-slider-nav-right"><span class="linea-arrows-slim-right"></span></button>',
+					autoplay: false,
+					accessibility: false,
+					customPaging: function (slider, i) {
+						return '<a href="#">' + $('.prod_single_thumbs_slider li:nth-child(' + (i + 1) + ')').html() + '</a>';
+					}
+				});
+			}
+
+
 
 	} // initSliders
 
@@ -987,13 +1034,13 @@ function validateField ( field ) {
 	(function($){
 
 
-		$(window).on("load",function(){
+		/*$(window).on("load",function(){
 			$("#mCSB_1").mCustomScrollbar({
 				scrollButtons:{
 					enable:true
 				}
 			});
-		});
+		});*/
 
 
 	})(jQuery);
