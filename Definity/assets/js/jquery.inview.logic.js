52051d3f2062fd9a51ac6c07b80209a75349c132
Gregory Lewis <eternal191@gmail.com>
2019-03-21 19:13:11 -0400

    made small changes before uploading.

--- Definity/assets/js/jquery.inview.logic.js
+++ Definity/assets/js/jquery.inview.logic.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
     $('.portfolio-item.webdesing, .portfolio-item.print').on('inview', function(event, isInView) {
         if (isInView) {
-            // element is now visible in the viewport
-            console.log('is inview');
 
             $(this).addClass('animated fadeInRight delay-2s');
 
